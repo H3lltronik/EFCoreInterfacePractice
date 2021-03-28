094f15606e97f1737380fac7844a0ecde688a5d7
H3lltronik <esau.egs1@gmail.com>
2021-03-27 21:31:37 -0600

    New base

---
+++ Db/MysqlDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Db {
+    public class MysqlDbContext : DbContext {
+        //public DbSet<Waifu> Waifus;
+
+        public MysqlDbContext() : base() {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseMySQL("server=localhost;database=library;user=user;password=password");
+        }
+    }
+}
